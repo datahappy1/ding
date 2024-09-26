3608c9a8548df031fddb5da9a01d94be0739647d
datahappy1 <p.prudky@seznam.cz>
2024-09-26 23:56:34 +0200

    refactoring some more

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ding
 
 ping 'n' ding to improve the round trip time when fighting Wi-Fi black spots
+
+arguments:
+-default_host="www.seznam.cz"
+-max_iterations=20
--- main.go
+++ main.go
@@ -9,13 +9,19 @@ import (
 
 const DEFAULT_HOST = "www.seznam.cz"
 const MAX_ITERATIONS = 10
-const DEFAULT_TIME_MS = 100
-const TIME_TO_BEEP_DEFAULT_MULTIPLICATOR = 10
+const DEFAULT_TIME_MS = 10
+const TIME_TO_BEEP_DEFAULT_MULTIPLICATOR = 30
+
+var (
+	defaultHostFlag string
+	maxItersFlag    int
+)
 
 func main() {
-	var defaultHost = flag.String("default_host", DEFAULT_HOST, "default host")
-	var maxIters = flag.Int("max_iterations", MAX_ITERATIONS, "max iterations to proceed")
+	flag.StringVar(&defaultHostFlag, "default_host", DEFAULT_HOST, "default host")
+	flag.IntVar(&maxItersFlag, "max_iterations", MAX_ITERATIONS, "max iterations to proceed")
+	flag.Parse()
 
-	internal.Run(*defaultHost, *maxIters, DEFAULT_TIME_MS, TIME_TO_BEEP_DEFAULT_MULTIPLICATOR)
+	internal.Run(defaultHostFlag, maxItersFlag, DEFAULT_TIME_MS, TIME_TO_BEEP_DEFAULT_MULTIPLICATOR)
 	os.Exit(0)
 }
